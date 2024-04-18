d08e1a392ebfa5cf18b219dec3aa9bcbdaac97b1
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-19 02:29:52 +0900

    Title: 알파벳에 따른 평가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240419/알파벳에 따른 평가/evaluation-by-alphabet.cpp
+++ 240419/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -2,13 +2,13 @@
 #include <string>
 using namespace std;
 int main() {
-    string a;
+    char a;
     cin >> a;
-    if(a == "S") cout << "Superior";
-    else if(a == "A") cout << "Excellent";
-    else if(a == "B") cout << "Good";
-    else if(a == "C") cout << "Usually";
-    else if(a == "D") cout << "Effort";
+    if(a == 'S') cout << "Superior";
+    else if(a == 'A') cout << "Excellent";
+    else if(a == 'B') cout << "Good";
+    else if(a == 'C') cout << "Usually";
+    else if(a == 'D') cout << "Effort";
     else cout << "Failure";
     return 0;
 }

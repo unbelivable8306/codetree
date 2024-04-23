46072f2e9081417ebf35215063a115a1a07f8ce8
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-23 14:27:38 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240423/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 240423/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
-    if(a < b && a < c) cout << a;
-    if(b < c && b < a) cout << b;
-    if(c < a && c < b) cout << c;    
+    if(a <= b && a <= c) cout << a;
+    if(b <= c && b <= a) cout << b;
+    if(c <= a && c <= b) cout << c;    
     return 0;
 }

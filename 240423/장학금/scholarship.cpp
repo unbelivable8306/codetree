c738e4416045d89e7142f7a446edadc9a13eec65
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-23 15:07:48 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240423/장학금/scholarship.cpp
+++ 240423/장학금/scholarship.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     int a,b;
     cin >> a >> b;
-    if(a >= 90 && b>=95) cout << "10";
-    else if(a >= 90 && b>=90) cout << "5";
+    if(a >= 90 && b>=95) cout << "100000";
+    else if(a >= 90 && b>=90) cout << "50000";
     else cout << "0";
     return 0;
 }

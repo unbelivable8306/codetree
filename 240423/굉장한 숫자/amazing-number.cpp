ae7f9582b9450aaa4f4e8305ccfc2f588f6d6a4b
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-23 15:00:12 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240423/굉장한 숫자/amazing-number.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if((a % 2 == 1 && a % 3 == 0) || (a % 2 == 0 && a % 5 ==0)) cout << "true";
+    else cout << "false";
+    return 0;
+}

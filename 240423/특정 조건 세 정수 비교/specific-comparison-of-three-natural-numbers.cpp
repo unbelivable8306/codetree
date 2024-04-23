956ba1b281a7d16bfa76b435c1b7f0fbd479b26c
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-23 14:21:28 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240423/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 240423/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
-    if(a<b && a<c) cout << "1" << " ";
+    if(a<=b && a<=c) cout << "1" << " ";
     else cout << "0" << " ";
     if(a == b && b == c) cout << "1";
     else cout << "0";

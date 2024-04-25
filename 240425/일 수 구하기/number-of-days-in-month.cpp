25918b69dda0b70fd97ce3d0487c886ae83ab5b1
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-25 13:18:26 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240425/일 수 구하기/number-of-days-in-month.cpp
+++ 240425/일 수 구하기/number-of-days-in-month.cpp
@@ -4,7 +4,7 @@ int main() {
     int n;
     cin >> n;
     if(n == 1 || n == 3 || n == 5 || n == 7 || n == 8 || n == 10 || n == 12) cout << "31";
-    else if(n == 2) cout << "29";
+    else if(n == 2) cout << "28";
     else cout << "30";
     return 0;
 }

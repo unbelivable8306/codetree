6d5dc5b92dfc0ddef5647c76c0b921fb835df684
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-25 12:43:26 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240425/두 사람/two-person.cpp
+++ 240425/두 사람/two-person.cpp
@@ -5,7 +5,7 @@ int main() {
     char a_name, b_name;
     cin >> a >> a_name;
     cin >> b >> b_name;
-    if(((a_name == 'M') && (a >= 19)) || ((b_name == 'M') && (a >= 19))) cout << "1";
+    if(((a_name == 'M') && (a >= 19)) || ((b_name == 'M') && (b >= 19))) cout << "1";
     else cout << "0";
     return 0;
 }

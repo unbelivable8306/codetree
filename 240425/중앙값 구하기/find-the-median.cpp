0caceb778017f3def53a2b77878b71391309f753
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-25 13:50:01 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240425/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a>=b)
+        if(b>=c) cout << b;
+        else if(a>=c) cout << c;
+        else cout << a;
+    else
+        if(a>=c) cout << a;
+        else if(b>=c) cout << c;
+        else cout << b;
+    return 0;
+}

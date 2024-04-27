a131474736ba6971ceea925649191b9f3f920616
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-27 14:48:16 +0900

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240427/홀수만 더하기/add-only-odd-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int a;
+    int sum = 0;
+    for(int i=1;i<=n;i++){
+        cin >> a;
+        sum += a;
+    }
+    cout << sum;
+    return 0;
+}

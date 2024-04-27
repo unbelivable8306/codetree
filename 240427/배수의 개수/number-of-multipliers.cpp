1616edea960430e99ad7f1f1d398831f15a4bc41
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-27 14:14:45 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240427/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int cnt1=0;
+    int cnt2=0;
+    for(int i=0;i<10;i++){
+        cin >> n;
+        if(n%3 == 0) cnt1++;
+        if(n%5 == 0) cnt2++;
+    }
+    cout << cnt1 << " " << cnt2;
+    return 0;
+}

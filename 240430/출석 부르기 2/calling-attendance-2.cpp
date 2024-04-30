3175c637b31e49385fffbc4273f2c6360f1f138a
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-04-30 14:50:21 +0900

    Title: 출석 부르기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240430/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	while (1) {
+		cin >> a;
+		if (a == 1) cout << "John" << endl;
+		else if (a == 2) cout << "Tom" << endl;
+		else if (a == 3) cout << "Paul" << endl;
+		else if (a == 4) cout << "Sam" << endl;
+		else {
+			cout << "Vacancy";
+			break;
+		}
+		}
+	
+	return 0;
+}

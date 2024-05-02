c133fa979139143b58867fdd8a970ddfe7b6cb35
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-05-02 17:19:33 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240502/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    bool satisfied = false;
+    cin >> n;
+    for (int i = 2; i <= n-1; i++) {
+        if (n % i == 0) satisfied = true;
+    }
+    if (satisfied == true) cout << "C";
+    else cout << "N";
+    return 0;
+}

b80cc10b39a70a653d45068593e9606a841949cf
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-05-08 17:54:49 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240508/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int n,chpoint;
+int cnt = 0;
+int a[100];
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] == 2) cnt++;
+        if (cnt == 3) {
+            chpoint = i + 1;
+            cnt = 0;
+        }
+    }
+    cout << chpoint;
+    return 0;
+}

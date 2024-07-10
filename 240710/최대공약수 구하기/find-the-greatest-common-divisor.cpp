8b745a5b47fcea5884f120aea22de62556aec209
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-07-10 11:08:59 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240710/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void print_sq(int n, int m) {
+    int max = 0;
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0 && m % i == 0) {
+            max = i;
+        }
+    }
+    cout << max;
+}
+int main() {
+    int n,m;
+    cin >> n >> m ;
+    print_sq(n,m);
+    return 0;
+}

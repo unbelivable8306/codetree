d6a134ef0695c2fcad3ebd6b6197ff8dd0c078d0
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-07-22 22:22:39 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include<functional>
+using namespace std;
+
+vector<int> s;
+
+int main() {
+    int n,m;
+    cin >> n;
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> m;
+        s.push_back(m);
+    }
+    sort(s.begin(), s.end());
+    for (int i = 0; i < n; i++) {
+        cout << s[i] << " ";
+    }
+    cout << endl;
+    sort(s.begin(), s.end(), greater<int>());
+    for (int i = 0; i < n; i++) {
+        cout << s[i] << " ";
+    }
+    return 0;
+}

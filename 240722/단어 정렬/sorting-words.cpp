ceda36a802bb5ce65efc6401bade3940aa3ba611
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-07-22 22:51:15 +0900

    Title: 단어 정렬, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/단어 정렬/sorting-words.cpp
+++ 240722/단어 정렬/sorting-words.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include<string>
 using namespace std;
-string s[100];
+vector<string> s;
 int main() {
     int n;
+    string m;
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> s[i];
+        cin >> m;
+        s.push_back(m);
     }
-    sort(s, s + n);
+    sort(s.begin(), s.end());
     for (int i = 0; i < n; i++) {
         cout << s[i] << endl;
     }

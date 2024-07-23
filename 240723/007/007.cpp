a368037f9baf3b58a7253f962f6939db3c1f9440
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-07-23 14:21:11 +0900

    Title: 007, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/007/007.cpp
+++ 240723/007/007.cpp
@@ -1,27 +1,17 @@
 #include<iostream>
 #include<string>
+#include<tuple>
 using namespace std;
 
-class m {
-public:
-    string secret_code;
-    char meeting_point;
-    int time;
-
-    m(string sc, char mp, int t) {
-        this->secret_code = sc;
-        this->meeting_point = mp;
-        this->time = t;
-    }
-};
+tuple<string, char, int> t;
 
 int main() {
-    string s;
-    char c;
-    int t;
-    cin >> s >> c >> t;
-    m m1 = m(s, c, t);
-    cout << "secret code : " << m1.secret_code << endl;
-    cout << "meeting point : " << m1.meeting_point << endl;
-    cout << "time : " << m1.time << endl;
+    string a;
+    char b;
+    int c;
+    cin >> a >> b >> c;
+    t = make_tuple(a, b, c);
+    cout << "secret code : " << get<0>(t) << endl;
+    cout << "meeting point : " << get<1>(t) << endl;
+    cout << "time : " << get<2>(t) << endl;
 }

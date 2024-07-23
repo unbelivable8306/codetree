2ed1045c7c45e07f2d6d72fc749ae6a33152c450
unbelivable8306 <167340195+unbelivable8306@users.noreply.github.com>
2024-07-23 14:54:57 +0900

    Title: Next Level, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/Next Level/next-level.cpp
+++ 240723/Next Level/next-level.cpp
@@ -3,23 +3,14 @@
 #include<tuple>
 using namespace std;
 
-class f {
-public:
-    string id;
-    int level;
-    f(string a = "codetree", int lv = 10) {
-        this->id = a;
-        this->level = lv;
-    }
-};
+tuple<string, int> t;
 
 int main() {
     string a;
     int b;
     cin >> a >> b;
-    f f1 = f();
-    cout << "user" << " " << f1.id << " " << "lv" << " " << f1.level << endl;
-    f1.id = a;
-    f1.level = b;
-    cout << "user" << " " << f1.id << " " << "lv" << " " << f1.level << endl;
+    t = make_tuple("codetree", 10);
+    cout << "user" << " " << get<0>(t) << " " << "lv" << " " << get<1>(t) << endl;
+    t = make_tuple(a, b);
+    cout << "user" << " " << get<0>(t) << " " << "lv" << " " << get<1>(t) << endl;
 }
